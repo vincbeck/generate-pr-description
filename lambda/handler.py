770bb3c4c47769269c6a6001cc5d2f4bf58fb753
vincbeck <vincbeck@amazon.com>
2024-03-14 15:01:45 -0400

    Fix lambda handler

--- lambda/handler.py
+++ lambda/handler.py
@@ -13,13 +13,19 @@ def handler(event, context):
     logger.info('## ENVIRONMENT VARIABLES: %s', json.dumps(dict(**os.environ)))
     logger.info('## EVENT: %s', json.dumps(event))
 
-    if "url" not in event or ("queryStringParameters" not in event and "url" not in event["queryStringParameters"]):
+    url = None
+    if "url" in event:
+        # When executing directly the lambda
+        url = event["url"]
+    if "queryStringParameters" in event and "url" in event["queryStringParameters"]:
+        # When executing the lambda through its public URL
+        url = event["queryStringParameters"]["url"]
+
+    if not url:
         raise ValueError("'url' is required")
 
-    url = event.get("url", event["queryStringParameters"]["url"])
-
     code_diff = pull_code_diff(url)
-    logger.info("Generating description for PR %s ...", event["url"])
+    logger.info("Generating description for PR %s ...", url)
     description = generate_description(code_diff)
     logger.info("Description: %s", description)
 

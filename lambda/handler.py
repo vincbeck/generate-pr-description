dde25fe0f91f4a559a81120a2159d3b368e30bed
vincbeck <vincbeck@amazon.com>
2024-03-14 14:55:02 -0400

    Update the lambda handler to handler requests from Lambda URL calls

--- lambda/handler.py
+++ lambda/handler.py
@@ -13,10 +13,12 @@ def handler(event, context):
     logger.info('## ENVIRONMENT VARIABLES: %s', json.dumps(dict(**os.environ)))
     logger.info('## EVENT: %s', json.dumps(event))
 
-    if "url" not in event:
+    if "url" not in event or ("queryStringParameters" not in event and "url" not in event["queryStringParameters"]):
         raise ValueError("'url' is required")
 
-    code_diff = pull_code_diff(event["url"])
+    url = event.get("url", event["queryStringParameters"]["url"])
+
+    code_diff = pull_code_diff(url)
     logger.info("Generating description for PR %s ...", event["url"])
     description = generate_description(code_diff)
     logger.info("Description: %s", description)
